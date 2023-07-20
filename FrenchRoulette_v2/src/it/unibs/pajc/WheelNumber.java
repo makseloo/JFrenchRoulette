04fa0c570c92b745b3a8698caf0734665d86df47
maks <maksymilianle@gmail.com>
2023-07-20 18:18:52 +0200

    offline statistics
    geneatating statistics without worrying to how to send them to clients

--- FrenchRoulette_v2/src/it/unibs/pajc/WheelNumber.java
+++ FrenchRoulette_v2/src/it/unibs/pajc/WheelNumber.java
@@ -18,6 +18,21 @@ public class WheelNumber implements Serializable {
 	public static List<Integer> zero = Arrays.asList(12, 35, 3, 26, 0, 32, 15);
 	//numbers order based on their position on the wheel
 	public static List<Integer> numbers = Arrays.asList(0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26);
+	//dozens
+	public static List<Integer> dozen1 = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12);
+	public static List<Integer> dozen2 = Arrays.asList(13,14,15,16,17,18,19,20,21,22,23,24);
+	public static List<Integer> dozen3 = Arrays.asList(25,26,27,28,29,30,31,32,33,34,35,36);
+	//cols
+	public static List<Integer> col1 = Arrays.asList(1,4,7,10,13,16,19,22,25,28,31,34);
+	public static List<Integer> col2 = Arrays.asList(2,5,8,11,14,17,20,23,26,29,32,35);
+	public static List<Integer> col3 = Arrays.asList(3,6,9,12,15,18,21,24,27,30,33,36);
+	//1-18 19-36
+	public static List<Integer> eight = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18);
+	public static List<Integer> six = Arrays.asList(19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36);
+	//even odd
+	public static List<Integer> even = Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16, 18,20, 22, 24, 26, 28, 30, 32, 34, 36);
+	public static List<Integer> odd = Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15, 17,19, 21, 23, 25, 27, 29, 31, 33, 35);
+	
 	
 	private int value;
 	private String zone;
@@ -78,5 +93,50 @@ public class WheelNumber implements Serializable {
 		return bettedValue;
 	}
 
+	public static List<Integer> getDozen1() {
+		return dozen1;
+	}
 
+	public static List<Integer> getDozen2() {
+		// TODO Auto-generated method stub
+		return dozen2;
+	}
+
+	public static List<Integer> getDozen3() {
+		return dozen3;
+	}
+
+	public static List<Integer> getCol1() {
+		return col1;
+	}
+
+	public static List<Integer> getCol2() {
+		return col2;
+	}
+
+	public static List<Integer> getCol3() {
+		return col3;
+	}
+
+	public static List<Integer> getEight() {
+		return eight;
+	}
+
+	public static List<Integer> getSix() {
+		return six;
+	}
+
+	public static List<Integer> getEven() {
+		return even;
+	}
+
+	public static List<Integer> getOdd() {
+		return odd;
+	}
+
+	public String getZone() {
+		return zone;
+	}
+
+	
 }
--- FrenchRoulette_v2/src/it/unibs/pajc/server/ServerStatistics.java
+++ FrenchRoulette_v2/src/it/unibs/pajc/server/ServerStatistics.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
+import it.unibs.pajc.WheelNumber;
+
 public class ServerStatistics {
-    private static final int NUMBER_OF_RANDOMS = 500;
+    private static double NUMBER_OF_RANDOMS = 20;
     private static final int RANDOM_RANGE_MIN = 0;
     private static final int RANDOM_RANGE_MAX = 37;
     
@@ -23,8 +25,104 @@ public class ServerStatistics {
 		stats.put("3dozen", 30);
 	}
 
+    public static void main(String[] args) {
+    	ServerStatistics server = new ServerStatistics();
+    	int[] randomNumbers = server.getNumbers();
+    	
+    	double zero = countOccurrences(randomNumbers, 0);
+    	
+    	double occInDoz1 = countOccurrencesInZone(randomNumbers, WheelNumber.getDozen1());
+    	double occInDoz2 = countOccurrencesInZone(randomNumbers, WheelNumber.getDozen2());
+    	double occInDoz3 = countOccurrencesInZone(randomNumbers, WheelNumber.getDozen3());
+    	
+    	double occInCol1 = countOccurrencesInZone(randomNumbers, WheelNumber.getCol1());
+    	double occInCol2 = countOccurrencesInZone(randomNumbers, WheelNumber.getCol2());
+    	double occInCol3 = countOccurrencesInZone(randomNumbers, WheelNumber.getCol3());
+    	
+    	double occInTier = countOccurrencesInZone(randomNumbers, WheelNumber.getTier());
+    	double occInOrph = countOccurrencesInZone(randomNumbers, WheelNumber.getOrphelins());
+    	double occInVois = countOccurrencesInZone(randomNumbers, WheelNumber.getVoisins());
+    	double occInZero = countOccurrencesInZone(randomNumbers, WheelNumber.getZero());
+    	
+    	double occIn1To18 = countOccurrencesInZone(randomNumbers, WheelNumber.getEight());
+    	double occIn19To36 = countOccurrencesInZone(randomNumbers, WheelNumber.getSix());
+    
+    	double occInEven = countOccurrencesInZone(randomNumbers, WheelNumber.getEven());
+    	double occInOdd = countOccurrencesInZone(randomNumbers, WheelNumber.getOdd());
+    	
+    	double occInBlack = countOccurrencesInZone(randomNumbers, WheelNumber.getBlackNums());
+    	double occInRed = countOccurrencesInZone(randomNumbers, WheelNumber.getRedNums());
+    	
+
+    	
+    	for(int i : randomNumbers) {
+    		System.out.print(","+ i);
+    	}
+    	System.out.print("\n occurence of 0: "+ countOccurrences(randomNumbers, 0));
+    	System.out.print("\n occurence of first dozen: "+ occInDoz1);
+    	System.out.println("\n" + calculatePercentage(occInDoz1));
+    }
+
+
+	private static List generateRandomNumbers(double count, int min, int max) {
+        int[] randomNumbers = new int[(int) count];
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            randomNumbers[i] = random.nextInt(max - min + 1) + min;
+        }
+        
+        return randomNumbers;
+    }
+    
+    private static int countOccurrences(int[] numbers, int target) {
+    	
+    	int count = 0;
+        for (int num : numbers) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    private static int calculatePercentage(double occurence) {
+    	double res = (occurence/NUMBER_OF_RANDOMS) * 100;
+        return (int)res;
+    }
+    //dozzine
+    private static int countOccurrencesInZone(int[] numbers, List<Integer> targetZone) {
+        int count = 0;
+
+        for (int num : numbers) {
+            for (int target : targetZone) {
+                if (num == target) {
+                    count++;
+                    break; // Optimization: stop searching if the number is found
+                }
+            }
+        }
+        return count;
+    }
+    
+    
+    //per più tardi quando c'è da giocare
+
+    private int generateSingleNumber(int min, int max) {
+    	Random random = new Random();
+    	return random.nextInt(max - min + 1) + min;
+    }
+
+    public HashMap<String, Integer> getStats(){
+    	return this.stats;
+    }
+    
 	public int getZero() {
 		return zero;
+	}
+
+	public int[] getNumbers() {
+		return randomNumbers;
 	}
 
 	public void setZero(int zero) {
@@ -38,67 +136,6 @@ public class ServerStatistics {
 	public void setDozens(int[] dozens) {
 		this.dozens = dozens;
 	}
-
-	private static int[] generateRandomNumbers(int count, int min, int max) {
-        int[] randomNumbers = new int[count];
-        Random random = new Random();
-
-        for (int i = 0; i < count; i++) {
-            randomNumbers[i] = random.nextInt(max - min + 1) + min;
-        }
-
-        return randomNumbers;
-    }
-    
-    //statistiche da fare, calcola lo zero, e poi te ne bastano 1 o due e basta che sottrai
-    /*
-    *0
-    *dozzine
-    *colonne
-    *zone
-    *1-18 e 19-36
-    *pari dispari
-    *rosso nero
-    */
-    
-    private int countOccurrences(int[] numbers, int target) {
-    	
-    	int count = 0;
-        for (int num : numbers) {
-            if (num == target) {
-                count++;
-            }
-        }
-        return count;
-    }
-    
-    
-    private static int countOccurrencesInDozen(int[] numbers, List<Integer> targetDozen) {
-        int count = 0;
-
-        for (int num : numbers) {
-            for (int target : targetDozen) {
-                if (num == target) {
-                    count++;
-                    break; // Optimization: stop searching if the number is found
-                }
-            }
-        }
-        return count;
-    }
-    
-    
-    
-    //per più tardi quando c'è da giocare
-
-    private int generateSingleNumber(int min, int max) {
-    	Random random = new Random();
-    	return random.nextInt(max - min + 1) + min;
-    }
-
-    public HashMap<String, Integer> getStats(){
-    	return this.stats;
-    }
 
 }
 
